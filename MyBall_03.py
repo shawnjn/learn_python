617a9b33abfd20e5d2826e184f331110faa79cd8
shawnjn <58937805+shawnjn@users.noreply.github.com>
2020-01-05 23:39:19 +0800

    Update MyBall_03.py

--- MyBall_03.py
+++ MyBall_03.py
@@ -11,7 +11,8 @@ class MyBallClass(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
         self.speed = speed
-
+        
+"""撞击使用的是rect，非像素"""
     def move(self):
         self.rect = self.rect.move(self.speed)
         if self.rect.left < 0 or self.rect.right > width:
@@ -35,12 +36,12 @@ def animate(group):
     # pygame.time.delay(20)
 
 
-#主程序
+"""主程序"""
 size = width, height = 640, 480
 screen = pygame.display.set_mode(size)
 screen_name = pygame.display.set_caption('球之间的碰撞')
 screen.fill([255, 255, 255])
-                                    #这里程序多写 pygame.display.flip()
+                                    #学习时，这里程序多写 pygame.display.flip()导致报错
 img_file = "beach_ball.png"
 clock = pygame.time.Clock()         #line 34删除pygame.time.delay()后使用的
 group = pygame.sprite.Group()
@@ -53,6 +54,8 @@ for row in range(0, 2):
         ball = MyBallClass(img_file, location, speed)
         # balls.append(ball)
         group.add(ball)
+        
+ """这里是创建多个程序运行时使用的，此处注释掉了"""
 # for ball in balls:
 #     screen.blit(ball.image, ball.rect)
 # pygame.display.flip()
@@ -61,10 +64,12 @@ for row in range(0, 2):
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:           # 编写时会，注意防止出现pygame.quit()的情况
             frame_rate = clock.get_fps()        # 检测帧数
             print("frame rate = ", frame_rate)  # 检测帧数输出
             running = False
+            
+    """注释掉程序主要是创建多个球时使用"""
     # pygame.time.delay(20)
     # screen.fill([255, 255, 255])
     # for ball in balls:
